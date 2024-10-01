886dd593155bd223b343f390daee1b9536f4aed2
Oshonik Kumar <workforoshonik@gmail.com>
2024-10-01 13:36:50 -0500

    1:36 PM

---
+++ demos/week6/main.py
@@ -0,0 +1,11 @@
+# Define a function, main is typically used to indicate where a program starts
+def main():
+    print("Main")
+
+# Define a function
+def sayHello():
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
