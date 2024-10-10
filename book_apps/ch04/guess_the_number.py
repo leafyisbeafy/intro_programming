331329f6db5b997ee464d5d366f8986e20e521a5
Oshonik Kumar <workforoshonik@gmail.com>
2024-10-10 12:46:46 -0500

    book changes

--- book_apps/ch04/guess_the_number.py
+++ book_apps/ch04/guess_the_number.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
 import random
+from random import randint
 
-LIMIT = 10
+LIMIT = 10  # fixed or constant variable
+
 
 def display_title():
     print("Guess the number!")
     print()
 
-def play_game():    
-    number = random.randint(1, LIMIT)
+
+def play_game():
+    number = random.randint(1, LIMIT)  # generates a random number between 1 and 10
     print(f"I'm thinking of a number from 1 to {LIMIT}\n")
     count = 1
 
     while (guess := int(input("Your guess: "))) != number:
         if guess < number:
             print("Too low.")
-            count += 1
         elif guess > number:
             print("Too high.")
-            count += 1
-    print(f"You guessed it in {count} tries.\n")
-     
+        count += 1
+    print(f"You guessed it in {count} ", end="")
+    print("try." if count == 1 else "tries.")
+
+
 def main():
     display_title()
     again = "y"
@@ -30,6 +34,7 @@ def main():
         again = input("Would you like to play again? (y/n): ")
         print()
     print("Bye!")
+
 
 # if started as the main module, call the main function
 if __name__ == "__main__":
